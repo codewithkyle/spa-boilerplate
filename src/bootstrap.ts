5be481b15c6c02c9d3c7c62399af733362e055b5
Kyle Andrews <codingwithkyle@gmail.com>
2021-07-27 19:01:28 -0400

    Relocated router, added SW + asset manager, added production build command

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,15 +1,9 @@
-import { configure, mount } from "@codewithkyle/router";
 import db from "@codewithkyle/jsql";
 
 (async () => {
-// @ts-ignore
-await db.start();
+    // @ts-ignore
+    await db.start();
 
-const main = document.body;
-mount(main);
-
-configure({
-    "/": "home-page",
-    "404": "missing-page",
-});
+    //@ts-ignore
+    await import("/js/router.js");
 })();
---
+++ src/router.ts
@@ -0,0 +1,9 @@
+import { configure, mount } from "@codewithkyle/router";
+
+const main = document.body;
+mount(main);
+
+configure({
+    "/": "home-page",
+    "404": "missing-page",
+});
